602cb005ae4de3236be51355c6565718a2e4e136
Gerson Sanchez <gsanchez@x-pertec.com>
2024-01-27 14:01:18 -0500

    listar blog del api

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Banner from './Banner'
-import PostCard from '../blogs/PostCard'
+import PostCards from '../blogs/PostCards'
 import Category from './Category'
 
 const Home = () => {
@@ -12,7 +12,7 @@ const Home = () => {
       {/* ----- Boddy ----- */}
       <div className="block lg:flex lg:space-x-2 px-2 lg:p-0 mt-10 mb-10">
         {/* post cards */}
-        <PostCard />
+        <PostCards />
 
         {/* right sidebar */}
         <Category />
